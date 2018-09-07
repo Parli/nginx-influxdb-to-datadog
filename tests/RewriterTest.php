58fe0e14231240d6f474765e0903a485417baec9
Eric Stern <eric@slant.co>
2018-09-07 11:29:59 -0700

    Make tests useful

--- tests/RewriterTest.php
+++ tests/RewriterTest.php
@@ -12,13 +12,17 @@ use DataDog\DogStatsd;
  */
 class RewriterTest extends \PHPUnit\Framework\TestCase
 {
-    private $increments = [];
+    private $expectedIncrements = [];
+    private $expectedHistograms = [];
+
     private $histograms = [];
+    private $increments = [];
+
     /**
      * @covers ::rewrite
      * @dataProvider influxDbMessages
      */
-    public function testRewrite(string $message, array $tags)
+    public function testRewrite(string $message, string $prefix, array $tags)
     {
         $dd = $this->createMock(DogStatsd::class);
         $dd->method('histogram')
@@ -32,15 +36,21 @@ class RewriterTest extends \PHPUnit\Framework\TestCase
             });
         $rewriter = new Rewriter($dd);
         $rewriter->rewrite($message);
-        var_dump($this->increments);
-        var_dump($this->histograms);
+
+        $this->expectIncrement('request.count');
+        $this->performAssertions($prefix, $tags);
     }
 
     public function influxDbMessages(): array
     {
         return [
             [
-                'default,server_name=amp.reefpig.com method="GET",status=500,bytes_sent=248,body_bytes_sent=21,header_bytes_sent=227,request_length=833,uri="/questions/2000",extension="",content_type="text/plain; charset=utf-8",request_time=0.047',
+                'default,server_name=amp.reefpig.com method="GET",status=500,'.
+                'bytes_sent=248,body_bytes_sent=21,header_bytes_sent=227,'.
+                'request_length=833,uri="/questions/2000",extension="",'.
+                'content_type="text/plain; charset=utf-8",request_time=0.047',
+
+                'default',
                 ['code' => '500',
                  'method' => 'GET',
                  'server_name' => 'amp.reefpig.com',
@@ -48,7 +58,35 @@ class RewriterTest extends \PHPUnit\Framework\TestCase
             ],
         ];
     }
-}
-ingress-nginx-transformed,server_name=amp.reefpig.com method="GET",status=404,bytes_sent=223,body_bytes_sent=9,header_bytes_sent=214,request_length=799,uri="/f,oo"bar",extension="",content_type="text/plain; charset=utf-8",request_time=0.001
 
-ingress-nginx-transformed,server_name=amp.reefpig.com method="GET",status=404,bytes_sent=223,body_bytes_sent=9,header_bytes_sent=214,request_length=824,uri="/foo"bar",extension="",content_type="text/plain; charset=utf-8",request_time=0.003
+    private function expectIncrement(string $metric)
+    {
+        $this->expectedIncrements[] = $metric;
+    }
+
+    private function performAssertions(string $prefix, array $tags)
+    {
+        $this->assertCount(count($this->expectedIncrements), $this->increments, 'Incorrect increment count');
+        $this->assertCount(count($this->expectedHistograms), $this->histograms, 'Incorrect histogram count');
+
+        foreach ($this->expectedIncrements as $suffix) {
+            $metric = sprintf('%s.%s', $prefix, $suffix);
+            $found = false;
+            foreach ($this->increments as $increment) {
+                if ($increment[0] === $metric) {
+                    $found = $increment;
+                    break;
+                }
+            }
+            if (!$found) {
+                $this->fail(sprintf('Expected increment metric %s not found', $metric));
+            }
+            ksort($tags);
+            ksort($found[2]);
+            $this->assertEquals($tags, $found[2], 'Tags do not match');
+        }
+    }
+}
+// metric,server_name=amp.reefpig.com method="GET",status=404,bytes_sent=223,body_bytes_sent=9,header_bytes_sent=214,request_length=799,uri="/f,oo"bar",extension="",content_type="text/plain; charset=utf-8",request_time=0.001
+
+// metric,server_name=amp.reefpig.com method="GET",status=404,bytes_sent=223,body_bytes_sent=9,header_bytes_sent=214,request_length=824,uri="/foo"bar",extension="",content_type="text/plain; charset=utf-8",request_time=0.003
